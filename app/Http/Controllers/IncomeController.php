1fecb47b0ecc1b6858a233eca03a8cf6abc1e7af
silentwasd <silentwasd@gmail.com>
2022-08-30 12:15:27 +0300

    Refactor IncomeController and make stats of total values more convenient to month stats.

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -7,6 +7,7 @@ use App\Structures\Money;
 use App\Structures\Month;
 use DateTime;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use UnitEnum;
@@ -33,6 +34,31 @@ class IncomeController extends Controller
         $firstDay = new Carbon(new DateTime("first day of $month->name"));
         $lastDay = new Carbon(new DateTime("last day of $month->name"));
 
+        $completed = $this->groupedByCompletedTime($firstDay, $lastDay);
+
+        $total = [
+            'sum' => new Money( $completed->sum(fn (array $expense) => $expense['sum']->pennies()) ?? 0 ),
+            'min' => new Money( $completed->min(fn (array $expense) => $expense['sum']->pennies()) ?? 0 ),
+            'max' => new Money( $completed->max(fn (array $expense) => $expense['sum']->pennies()) ?? 0 )
+        ];
+
+        $total['avg'] = new Money( $total['sum']->pennies() / now()->daysInMonth );
+
+        $months = collect(Month::cases());
+        $curMonthKey = $months->search(fn (UnitEnum $unit) => $unit->name == $month->name);
+
+        return view('incomes.stats')
+            ->with('result', $this->groupedByType($firstDay, $lastDay))
+            ->with('total', $total)
+            ->with('months', [
+                'cur' => $month->value,
+                'next' => $curMonthKey + 1 < $months->count() ? $months[$curMonthKey + 1]->value : null,
+                'prev' => $curMonthKey - 1 >= 0 ? $months[$curMonthKey - 1]->value : null,
+            ]);
+    }
+
+    private function groupedByType(Carbon $firstDay, Carbon $lastDay): Collection
+    {
         $incomes = DB::table('incomes')
             ->selectRaw('sum(value) as value, min(value) as min, max(value) as max, count(value) as count, income_types.name as income_type')
             ->groupBy('income_type')
@@ -42,36 +68,24 @@ class IncomeController extends Controller
             ->orderByDesc('value')
             ->get();
 
-        $result = $incomes->map(fn (object $income) => [
+        return $incomes->map(fn (object $income) => [
             'value' => new Money($income->value),
             'type' => $income->income_type,
             'min' => new Money($income->min),
             'max' => new Money($income->max),
             'avg' => new Money($income->value / $income->count)
         ]);
+    }
 
-        $total = $result->count() > 0 ? [
-            'sum' => new Money( $result->sum(fn (array $income) => $income['value']->pennies()) ),
-            'min' => new Money( $result->min(fn (array $income) => $income['min']->pennies()) ),
-            'max' => new Money( $result->max(fn (array $income) => $income['max']->pennies()) )
-        ] : [
-            'sum' => new Money(0),
-            'min' => new Money(0),
-            'max' => new Money(0)
-        ];
+    private function groupedByCompletedTime(Carbon $firstDay, Carbon $lastDay): Collection
+    {
+        $incomes = DB::table('incomes')
+            ->selectRaw('sum(value) as sum')
+            ->groupBy('completed_at')
+            ->where('completed_at', '>=', $firstDay)
+            ->where('completed_at', '<=', $lastDay)
+            ->get();
 
-        $total['avg'] = new Money( $total['sum']->pennies() / now()->daysInMonth );
-
-        $months = collect(Month::cases());
-        $curMonthKey = $months->search(fn (UnitEnum $unit) => $unit->name == $month->name);
-
-        return view('incomes.stats')
-            ->with('result', $result)
-            ->with('total', $total)
-            ->with('months', [
-                'cur' => $month->value,
-                'next' => $curMonthKey + 1 < $months->count() ? $months[$curMonthKey + 1]->value : null,
-                'prev' => $curMonthKey - 1 >= 0 ? $months[$curMonthKey - 1]->value : null,
-            ]);
+        return $incomes->map(fn (object $income) => [ 'sum' => new Money($income->sum) ]);
     }
 }
