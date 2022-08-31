73419c4bbe0fe0dbd2a26d22c480cc0ee4bc188d
silentwasd <silentwasd@gmail.com>
2022-08-31 08:42:38 +0300

    Incomes by default.

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/incomes');
 
 Route::prefix('incomes')->name('incomes.')->group(function () {
 
