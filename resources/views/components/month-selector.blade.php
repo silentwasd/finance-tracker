a1895705b692e6a029fcb6bb5290f4883e86080d
silentwasd <silentwasd@gmail.com>
2022-09-14 08:45:38 +0300

    Month selector use icon font.

--- resources/views/components/month-selector.blade.php
+++ resources/views/components/month-selector.blade.php
@@ -1,19 +1,19 @@
-<div class="row">
+<div class="row align-items-center">
     <div class="col-auto">
         <a class="btn btn-primary {{ $prev == null ? 'disabled' : '' }}"
            href="{{ $prev ? route(Route::currentRouteName(), $prev) : '#' }}">
-            {{ __('month-calendar.prev') }}
+            <span class="fa fa-arrow-left"></span>
         </a>
     </div>
 
     <div class="col text-center">
-        <h3>{{ Str::ucfirst(__("months.$current")) }}</h3>
+        <h3 class="m-0">{{ Str::ucfirst(__("months.$current")) }}</h3>
     </div>
 
     <div class="col-auto">
         <a class="btn btn-primary {{ $next == null ? 'disabled' : '' }}"
            href="{{ $next ? route(Route::currentRouteName(), $next) : '#' }}">
-            {{ __('month-calendar.next') }}
+            <span class="fa fa-arrow-right"></span>
         </a>
     </div>
 </div>
